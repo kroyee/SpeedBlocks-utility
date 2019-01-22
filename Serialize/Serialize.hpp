76792ffe594336c03cce70bac4149c01c0821cbf
kroyee <olle.samuelsson@gmail.com>
2019-01-22 13:12:02 +0100

    Updated Serialize to support containers

--- Serialize/Serialize.hpp
+++ Serialize/Serialize.hpp
@@ -25,6 +25,12 @@ using has_right_stream = decltype(std::declval<LHS>() >> std::declval<RHS>());
 template <class LHS, class RHS>
 constexpr bool has_right_stream_v = os::detect_v<has_left_stream, LHS, RHS>;
 
+template <class T>
+using is_container = decltype(std::declval<T>().begin(), std::declval<T>().end(), std::declval<T>().size());
+
+template <class T>
+constexpr bool is_container_v = (os::detect_v<is_container, T> && !std::is_same_v<std::decay_t<T>, std::string>);
+
 template <class Stream>
 struct To {
     To(Stream& stream) : m_stream(stream) {}
@@ -32,24 +38,36 @@ struct To {
 
     template <class Object>
     To& operator<<(Object&& object) {
-        if constexpr (has_serialize_v<Object>) {
-            object.serialize([&](const auto&... args) { (*this << ... << args); });
+        if constexpr (is_container_v<Object>) {
+            *this << object.size();
+            for (auto& val : object) {
+                *this << val;
+            }
+        } else if constexpr (has_serialize_v<Object>) {
+            object.serialize([&](auto&&... args) { (*this << ... << args); });
         } else if constexpr (has_left_stream_v<Stream, Object>) {
             m_stream << object;
         } else {
-            os::bind_to_members([&](const auto&... args) { (*this << ... << args); }, object);
+            os::bind_to_members([&](auto&&... args) { (*this << ... << args); }, object);
         }
         return *this;
     }
 
     template <class Object>
     To& operator>>(Object&& object) {
-        if constexpr (has_serialize_v<Object>) {
-            object.serialize([&](const auto&... args) { (*this >> ... >> args); });
+        if constexpr (is_container_v<Object>) {
+            std::size_t count;
+            *this >> count;
+            object.resize(count);
+            for (auto& val : object) {
+                *this >> val;
+            }
+        } else if constexpr (has_serialize_v<Object>) {
+            object.serialize([&](auto&&... args) { (*this >> ... >> args); });
         } else if constexpr (has_left_stream_v<Stream, Object>) {
             m_stream >> object;
         } else {
-            os::bind_to_members([&](const auto&... args) { (*this >> ... >> args); }, object);
+            os::bind_to_members([&](auto&&... args) { (*this >> ... >> args); }, object);
         }
         return *this;
     }
